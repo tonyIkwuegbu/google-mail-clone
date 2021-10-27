7d31aeb0e40ec61f616fd8673f75f65b75f2aa1c
Anthony Ikwuegbu <ikwuegbuanthony02@gmail.com>
2021-10-27 14:55:09 +0100

    Updated Auth for login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Mail from "./Mail";
 import EmailList from "./EmailList";
+import SendMail from "./SendMail";
+import {useSelector, useDispatch} from "react-redux";
+import {selectSendMessageIsOpen} from "./features/mailSlice"
+import {login, selectUser} from "./features/userSlice"
+import Login from "./Login"
+import { auth } from "./Firebase";
+
 
 function App() {
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+useEffect(() => {
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      //user is logged in
+   dispatch(login({
+     displayName: user.displayName,
+     email: user.email,
+     photoUrl: user.photoURL
+   }))
+    }
+  })
+}, [dispatch])
+
   return (
     <Router>
+    {!user? (
+      <Login/>
+    ):(
       <div className="app">
         <Header />
         <div className="app_body">
@@ -18,7 +45,10 @@ function App() {
             <Route exact path="/" component={EmailList} />
           </Switch>
         </div>
+        {sendMessageIsOpen && <SendMail/>}
       </div>
+    )}
+      
     </Router>
   );
 }
